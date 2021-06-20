21db90d0e1377bacad7289b925a75c71035c70dc
Allan Ritchie <allan.ritchie@gmail.com>
2021-06-20 11:36:27 -0400

    WIP

---
+++ src/Shiny.Core/Infrastructure/ShinyLoggerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+
+namespace Shiny.Infrastructure
+{
+    public class ShinyLoggerFactory : ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return null;
+        }
+
+
+        public void Dispose() { }
+    }
+}
---
+++ src/Shiny.Core/ShinyLogHost.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+
+namespace Shiny
+{
+    public static class ShinyLogHost
+    {
+        static ILogger? defaultLogger;
+        public static ILogger? Default => defaultLogger ??= GetLog("Default");
+        public static ILogger<T>? GetLog<T>() => ShinyHost.LoggerFactory.CreateLogger<T>();
+        public static ILogger? GetLog(string categoryName) => ShinyHost.LoggerFactory.CreateLogger(categoryName);
+    }
+}
